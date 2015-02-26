78550bbece71c3dc5a09b7dbd0b92f6e70d1fb2f
Sergii Vorushylo <Sergii.Vorushylo@episerver.com>
2015-02-26 14:15:53 +0100

    Add a test for the case with duplicate property names in container object and nested object

--- Nested2Find.Tests/Stories/Nested.cs
+++ Nested2Find.Tests/Stories/Nested.cs
@@ -312,6 +312,118 @@ namespace Nested2Find.Stories
         }
     }
 
+    public class Nested4 : IDisposable
+    {
+        [Fact]
+        public void FilterByMultipleValuesForANestedListOfComplexObjectsWithDupplicatePropertyNamesUsingFilterDelegateBuilder()
+        {
+            new Story("Filter by matching a specific item in a list")
+                .InOrderTo("be able to filter by several values on objects in lists of complex objects having similar property names")
+                .AsA("developer")
+                .IWant("to be able to map list of complex objects as nested and filter by multiple values in a unique object in the list")
+                .WithScenario("mapping list of complex objects as nested")
+                .Given(IHaveAClient)
+                    .And(IHaveMappedIEnumerablePropertiesAsNested)
+                    .And(IHaveTwoCompanyObjects)
+                    .And(IHaveTwoDepartmentObjects)
+                    .And(TheFirstDepartmentHasAnEmployeeNamedCristianoRonaldo)
+                    .And(TheSecondDepartmentHasAnEmployeeNamedCristianoDoe)
+                    .And(TheSecondDepartmentHasAnEmployeeNamedJohnRonaldo)
+                    .And(TheFirstDepartmentBelongsToTheFirstCompany)
+                    .And(TheSecondDepartmentBelongsToTheSecondCompany)
+                    .And(IHaveIndexedTheCompanyObjects)
+                    .And(IHaveWaitedForASecond)
+                .When(ISearchForCompanyWithDepartmentWithAnEmployeeWithNameCristianoAndLastNameRonaldo)
+                .Then(IShouldGetASingleHit)
+                .And(ItShouldBeForTheFirstDepartment)
+                .Execute();
+        }
+
+        protected IClient client;
+        void IHaveAClient()
+        {
+            client = Client.CreateFromConfig();
+        }
+
+        void IHaveMappedIEnumerablePropertiesAsNested()
+        {
+            client.Conventions.AddNestedConventions();
+        }
+
+        private Company company1, company2;
+        void IHaveTwoCompanyObjects()
+        {
+            company1 = new Company("Company 1");
+            company2 = new Company("Company 2");
+        }
+
+        private Department department1, department2;
+        void IHaveTwoDepartmentObjects()
+        {
+            department1 = new Department("Department 1");
+            department2 = new Department("Department 2");
+        }
+
+        void TheFirstDepartmentHasAnEmployeeNamedCristianoRonaldo()
+        {
+            department1.Employees.Add(new Employee { Name = "Cristiano", LastName = "Ronaldo" });
+        }
+
+        void TheSecondDepartmentHasAnEmployeeNamedCristianoDoe()
+        {
+            department2.Employees.Add(new Employee { Name = "Cristiano", LastName = "Doe" });
+        }
+
+        void TheSecondDepartmentHasAnEmployeeNamedJohnRonaldo()
+        {
+            department2.Employees.Add(new Employee { Name = "John", LastName = "Ronaldo" });
+        }
+
+        void TheFirstDepartmentBelongsToTheFirstCompany()
+        {
+            company1.Departments.Add(department1);
+        }
+
+        void TheSecondDepartmentBelongsToTheSecondCompany()
+        {
+            company2.Departments.Add(department2);
+        }
+
+        void IHaveIndexedTheCompanyObjects()
+        {
+            client.Index(company1, company2);
+        }
+
+        void IHaveWaitedForASecond()
+        {
+            Thread.Sleep(1000);
+        }
+
+        SearchResults<Company> result;
+        void ISearchForCompanyWithDepartmentWithAnEmployeeWithNameCristianoAndLastNameRonaldo()
+        {
+            result = client.Search<Company>()
+                        .Filter(x => x.Departments.MatchItem(t => t.Employees.MatchItem(p => p.Name.Match("Cristiano") & p.LastName.Match("Ronaldo"))))
+                        .GetResult();
+        }
+
+        void IShouldGetASingleHit()
+        {
+            result.TotalMatching.Should().Be(1);
+        }
+
+        void ItShouldBeForTheFirstDepartment()
+        {
+            result.Single().Name.Should().Be(company1.Name);
+        }
+
+        public void Dispose()
+        {
+            client.Delete<Company>(x => x.Name.Match(company1.Name));
+            client.Delete<Company>(x => x.Name.Match(company2.Name));
+        }
+    }
+
     public class League
     {
         public League(string name)
@@ -338,5 +450,36 @@ namespace Nested2Find.Stories
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+    }
+
+    public class Company
+    {
+        public Company(string name)
+        {
+            Name = name;
+            Departments = new NestedList<Department>();
+        }
+
+        public string Name { get; set; }
+        public NestedList<Department> Departments { get; set; } 
+    }
+
+    public class Department
+    {
+        public Department(string name)
+        {
+            Name = name;
+            Employees = new NestedList<Employee>();
+        }
+
+        public string Name { get; set; }
+        public NestedList<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
     }
 }
